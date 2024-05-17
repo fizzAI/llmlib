7215c671b60f1572ca3c8dc2a76ef1f7912a490e
fizzAI <fizzarolli@riseup.net>
2024-05-17 18:00:50 -0400

    add gelu

---
+++ llmlib/activations/gelu.py
@@ -0,0 +1,29 @@
+# Hendrycks et. al 2016
+
+from jaxtyping import Array, Float, Int
+from typing import Optional
+import equinox as eqx
+import jax.numpy as jnp
+import jax
+
+class GELU(eqx.Module):
+    def __call__(self, x: Array):
+        return (
+            x / 2
+        ) * (
+            1 + jax.lax.erf(
+                x / jnp.sqrt(2)
+            )
+        )
+
+class ApproximateGELU(eqx.Module):
+    def __call__(self, x: Array):
+        return x * (
+            1 + jnp.tanh(
+                jnp.sqrt(
+                    2 / jnp.pi
+                ) * (
+                    x + 0.44715 * jnp.power(x, 3)
+                )
+            )
+        ) * 0.5
