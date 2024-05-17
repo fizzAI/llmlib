090897a0dc2164b76ac6dda292f357a438786bfb
fizzAI <fizzarolli@riseup.net>
2024-05-17 17:29:53 -0400

    add layernorn + slightly optimize rmsnorm

---
+++ llmlib/normalization/layernorm.py
@@ -0,0 +1,30 @@
+from jaxtyping import Array, Float, Int
+from typing import Optional
+import equinox as eqx
+import jax.numpy as jnp
+import jax
+
+class LayerNorm(eqx.Module):
+    shape: Int
+    epsilon: Float
+    weight: Optional[Array]
+    bias: Optional[Array]
+
+    def __init__(self, shape: Int, epsilon: Float = 1e-5, use_weight: bool = True, use_bias: bool = True):
+        self.shape = shape
+        self.weight = jnp.ones(shape) if use_weight else None
+        self.weight = jnp.ones(shape) if use_weight else None
+        self.epsilon = epsilon
+    
+    def __call__(self, x: Array, dtype=jnp.float32) -> Array:
+        x = jnp.asarray(x, dtype=dtype)
+        mean = jnp.mean(x, keepdims=True)
+        variance = jnp.var(x, keepdims=True)
+        variance = jnp.maximum(0.0, variance)
+        inv = jax.lax.rsqrt(variance + self.epsilon)
+        y = (x - mean) * inv
+        # FIXME: do i need double negation here
+        if not not self.weight:
+            y = y * self.weight
+        if not not self.bias:
+            y = y + self.bias
--- llmlib/normalization/rmsnorm.py
+++ llmlib/normalization/rmsnorm.py
@@ -1,7 +1,10 @@
 from jaxtyping import Array, Float, Int
 import equinox as eqx
 import jax.numpy as jnp
+import jax
 
+# FIXME: apparently the eqx impl supports a bias here? never seen it
+# Biao Zhang et. al
 class RMSNorm(eqx.Module):
     shape: Int
     scale: Array
@@ -15,6 +18,6 @@ class RMSNorm(eqx.Module):
     def __call__(self, x: Array, dtype=jnp.float32) -> Array:
         variance = jnp.asarray(x, dtype=dtype)
         variance = jnp.power(variance, 2).mean(axis=-1, keepdims=True)
-        x = x / jnp.sqrt(variance + self.epsilon)
+        x = x * jax.lax.rsqrt(variance + self.epsilon)
 
         return self.scale * x
